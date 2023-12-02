b238d0bdaa4a4b5c879dccd1019d17d6d877171f
Igor <42407016+everyvoid@users.noreply.github.com>
2023-12-02 17:48:45 +0100

    pixel ratio fixed

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setClearColor( 0xffffff, 1);
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+renderer.setPixelRatio(window.devicePixelRatio);
 
 
 // Geometry
